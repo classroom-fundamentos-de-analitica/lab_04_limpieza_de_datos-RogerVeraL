57379b4f556241542ee3a2b91ced39754485b626
Roger Vera Londoño <rveral@unal.edu.co>
2024-09-20 22:20:32 -0500

    lab04

--- pregunta.py
+++ pregunta.py
@@ -10,11 +10,29 @@ import pandas as pd
 
 
 def clean_data():
-
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
-
     #
     # Inserte su código aquí
     #
 
+    def monto_del_credito(df):
+        df["monto_del_credito"] = (
+            df["monto_del_credito"].str.strip().str.replace("[,$]|(\\.00$)", "", regex=True).astype(float)
+        )
+        return df
+    def fecha_de_beneficio(df):
+        df["fecha_de_beneficio"] = pd.to_datetime(df["fecha_de_beneficio"], format="%d/%m/%Y")
+        return df
+        
+
+    df = pd.read_csv("solicitudes_credito.csv", sep=";",index_col=0)
+    df = df.replace("-", " ", regex=True).replace("_", " ", regex=True)
+    df["sexo"] = df["sexo"].str.lower()
+    df["comuna_ciudadano"] = df["comuna_ciudadano"].astype(int)
+    df = df.drop_duplicates().dropna()
+    df = monto_del_credito(df)
+
     return df
+
+
+cleaned_data = clean_data()
+print(cleaned_data.head())
